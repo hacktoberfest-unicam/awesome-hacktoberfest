07c352753674164912741b0df07c2cf8449c9a2b
HarlockOfficial <harlockofficial.github@gmail.com>
2019-10-17 18:02:20 +0200

    Steganografia c#

---
+++ Steganografia/Steganografia/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace Steganografia
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.axWindowsMediaPlayer1 = new AxWMPLib.AxWindowsMediaPlayer();
+            this.buttonImporta = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // axWindowsMediaPlayer1
+            // 
+            this.axWindowsMediaPlayer1.Enabled = true;
+            this.axWindowsMediaPlayer1.Location = new System.Drawing.Point(12, 41);
+            this.axWindowsMediaPlayer1.Name = "axWindowsMediaPlayer1";
+            this.axWindowsMediaPlayer1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer1.OcxState")));
+            this.axWindowsMediaPlayer1.Size = new System.Drawing.Size(386, 324);
+            this.axWindowsMediaPlayer1.TabIndex = 0;
+            // 
+            // buttonImporta
+            // 
+            this.buttonImporta.Location = new System.Drawing.Point(12, 12);
+            this.buttonImporta.Name = "buttonImporta";
+            this.buttonImporta.Size = new System.Drawing.Size(75, 23);
+            this.buttonImporta.TabIndex = 1;
+            this.buttonImporta.Text = "Importa";
+            this.buttonImporta.UseVisualStyleBackColor = true;
+            this.buttonImporta.Click += new System.EventHandler(this.buttonImporta_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(424, 41);
+            this.richTextBox1.MaxLength = 6000;
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(355, 324);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Enabled = false;
+            this.button1.Location = new System.Drawing.Point(424, 371);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(355, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Cripta";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(785, 371);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(355, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Decripta";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(785, 41);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(355, 324);
+            this.richTextBox2.TabIndex = 4;
+            this.richTextBox2.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(424, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(136, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Numero Caratteri: 0  / 6000";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1204, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.buttonImporta);
+            this.Controls.Add(this.axWindowsMediaPlayer1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Steganografia";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer1;
+        private System.Windows.Forms.Button buttonImporta;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Steganografia/Steganografia/Form1.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Steganografia
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        byte[] _bytes;
+        byte[] _Cryptedbytes;
+        string _songPath = String.Empty;
+        string _outpath;
+        private void buttonImporta_Click(object sender, EventArgs e)
+        {
+
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+
+            openFileDialog1.InitialDirectory = "c:\\";
+            openFileDialog1.Filter = "MP3 files (*.mp3)|*.mp3";
+            openFileDialog1.FilterIndex = 0;
+            openFileDialog1.RestoreDirectory = true;
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                _songPath = openFileDialog1.FileName;
+                _bytes = File.ReadAllBytes(openFileDialog1.FileName);
+                button1.Enabled = true;
+                button2.Enabled = true;
+            }
+
+        }
+
+        public static byte[] GetBytes(string bitString)
+        {
+            return Enumerable.Range(0, bitString.Length / 8).
+                Select(pos => Convert.ToByte(
+                    bitString.Substring(pos * 8, 8),
+                    2)
+                ).ToArray();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button2.Enabled = false;
+            if (richTextBox1.Text.Length > 0 && !richTextBox1.Text.Contains("<EOS>"))
+            {
+                Crittografia steg = new Crittografia(_bytes, richTextBox1.Text + "<EOS>");
+                string _CryptedString = steg.Crypt();
+
+                if (!_CryptedString.Contains("Errore"))
+                {
+                    
+                    _Cryptedbytes = GetBytes(_CryptedString);
+
+                    try
+                    {
+                        using (MemoryStream ms = new MemoryStream(_Cryptedbytes))
+                        {
+                            FileStream file = new FileStream(_outpath, FileMode.Create, FileAccess.Write);
+                            ms.WriteTo(file);
+                            file.Close();
+                        }
+                    }catch(Exception ex)
+                    {
+                        MessageBox.Show(ex.Message, "Errore", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+
+
+                    axWindowsMediaPlayer1.URL = _outpath;
+                    button1.Enabled = false;
+                }
+                else
+                {
+                    MessageBox.Show(_CryptedString, "Errore", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+
+            }else
+            {
+                MessageBox.Show("Inserisci un testo valido", "Errore", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            button1.Enabled = false;
+            Crittografia steg = new Crittografia(_bytes,null);
+            string _DecryptedString = steg.Decrypt().Replace("<EOS>", "");
+
+            if(!_DecryptedString.Contains("Errore"))
+            {
+                richTextBox2.Text = _DecryptedString;
+                button2.Enabled = false;
+            }else
+            {
+                MessageBox.Show(_DecryptedString, "Errore", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+            label1.Text = "Numero Caratteri: " + richTextBox1.Text.Length + " / 6000";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if(!Directory.Exists(System.Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + @"/Crypted"))
+            {
+                Directory.CreateDirectory(System.Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + @"/Crypted");
+            }
+            _outpath = System.Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + @"/Crypted/CryptedMp3.mp3";
+        }
+    }
+}
