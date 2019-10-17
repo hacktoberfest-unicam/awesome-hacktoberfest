3d019bb1342cd038458fc14117b90e8f5caa3e04
matteoRondyUni <56674402+matteoRondyUni@users.noreply.github.com>
2019-10-17 13:08:30 +0200

    Creazione Sasso-Carta-Forbici in Java

---
+++ Sasso_Carta_Forbice/src/Partita.java
@@ -0,0 +1,70 @@
+
+public class Partita {
+	private int punteggioGiocatore = 0;
+	private int punteggioCPU = 0;
+
+	public void Start(Giocatore giocatoreUmano, Giocatore CPU) {
+		controlloTurno(giocatoreUmano.chiediMossa(), CPU.mossa());
+	}
+
+	private void controlloTurno(String g1, String g2) {
+		// Se il giocatore ha scelto Sasso
+		if (g1.equals("S"))
+			switch (g2) {
+			case "Sasso":
+				pareggio();
+				break;
+			case "Carta":
+				perdita();
+				break;
+			case "Forbici":
+				vincita();
+				break;
+			}
+		// Se il giocatore ha scelto Carta
+		if (g1.equals("C"))
+			switch (g2) {
+			case "Sasso":
+				vincita();
+				break;
+			case "Carta":
+				pareggio();
+				break;
+			case "Forbici":
+				perdita();
+				break;
+			}
+		// Se il giocatore ha scelto Foribici
+		if (g1.equals("F"))
+			switch (g2) {
+			case "Sasso":
+				perdita();
+				break;
+			case "Carta":
+				vincita();
+				break;
+			case "Forbici":
+				pareggio();
+				break;
+			}
+		if (g1.equals("0")) {
+			System.out.println("Errore: la tua scelta non è un valore accettabile");
+			System.out.println();
+		}
+	}
+
+	private void pareggio() {
+		System.out.println("Pareggio");
+		System.out.println();
+	}
+
+	private void perdita() {
+		System.out.println("Ha vinto la CPU");
+		System.out.println();
+	}
+
+	private void vincita() {
+		System.out.println("Hai vinto");
+		System.out.println();
+	}
+}
